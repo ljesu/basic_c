5cc3fa00a1e37cbdda2ef2e29bef1f1e5c718352
lorenzo <twitch.tv@laposte.net>
2017-07-03 15:19:16 +0200

    ouaiiiiii

---
+++ exercice2/exo.c
@@ -0,0 +1,47 @@
+#include<unistd.h>
+int main(void)
+{
+int i=9;
+if (i==0)
+{
+write(1,"0",1);
+}
+if (i==1)
+{
+write(1,"1",1);
+}
+if (i==2)
+{
+write(1,"2",1);
+}
+if (i==3)
+{
+write(1,"3",1);
+}
+if (i==4)
+{
+write(1,"4",1);
+}
+if (i==5)
+{
+write(1,"5",1);
+}
+if (i==6)
+{
+write(1,"6",1);
+}
+if (i==7)
+{
+write(1,"7",1);
+}
+if (i==8)
+{
+write(1,"8",1);
+}
+if (i==9)
+{
+write(1,"9",1);
+}
+
+return(0);
+}
